701d12d832f13c9e60f6aa6274bc23645dbed507
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-11-16 23:44:31 +0100

    Fast API template

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import json
+from typing import Literal
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Symptoms(BaseModel):
+    age: int
+    gender: Literal["male", "female"]
+    symptoms: list[str]
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to the Health Diagnosis RAG AI API"}
+
+
+@app.get("/diagnose")
+async def diagnose(symptoms: Symptoms):
+    return symptoms
