7aaf5350f1264b5e9ea4846835d95e9609323d2a
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-11-18 22:27:27 +0100

    Setup RAG documents, API and Agent
    - Process disease dataset CSV into JSONL format for better parsing.
    - Implement vector store loading logic with metadata support.
    - Define description for retrieval tool and system prompt for the agent.
    - Setup FastAPI endpoints for diagnosis.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import json
 from typing import Literal
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+from rag_agent import agent
+from langchain_core.messages import HumanMessage
 
 app = FastAPI()
 
 
-class Symptoms(BaseModel):
+class SymptomsInput(BaseModel):
     age: int
     gender: Literal["male", "female"]
     symptoms: list[str]
+
+
+class DiagnoseResponse(BaseModel):
+    possible_diseases: list[str]
 
 
 @app.get("/")
@@ -18,6 +22,12 @@ async def root():
     return {"message": "Welcome to the Health Diagnosis RAG AI API"}
 
 
-@app.get("/diagnose")
-async def diagnose(symptoms: Symptoms):
-    return symptoms
+@app.get("/diagnose", response_model=DiagnoseResponse)
+async def diagnose(symptoms: SymptomsInput):
+    prompt = f"Gender: {symptoms.gender}, age: {symptoms.age}, symptoms: {', '.join(symptoms.symptoms)}."
+    response = agent.invoke({
+        "messages": [HumanMessage(content=prompt)]
+    })["messages"][-1].content
+
+    possible_diseases = response.split(", ")
+    return DiagnoseResponse(possible_diseases)
